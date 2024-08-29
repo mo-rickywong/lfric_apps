4463b35a0c92b9f6e7cf4c210b02dacaf338a089
markhedley <enquiries@metoffice.gov.uk>
2024-08-29 14:08:35 +0000

    ticket:247 : terencevockerodt : Move building of PSyAD kernels into adjoint import
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@3349 3896877f-8c05-4358-bc88-7947bb4308c7

--- science/adjoint/build/build_psyad.mk
+++ science/adjoint/build/build_psyad.mk
@@ -6,13 +6,13 @@
 # Wrapper script to build the required PSyAD targets.
 # The PSyAD kernels are built using three stages.
 # 1) Pre-patch: copies (and patches) tangent linear kernels from their base dir to PSYAD_WDIR.
-# 2) PSyAD: generates adjoint kernel and adjoint test algorithm from the pre-patch stage tangent linear kernels.
+# 2) PSyAD: generates adjoint kernels and adjoint test algorithms from the pre-patch stage tangent linear kernels.
 # 3) Post-patch: copies (and patches) adjoint kernels and adjoint test algorithms from PSYAD_WDIR to WORKING_DIR.
 
 # List of targets needed by this script.
 include $(ADJOINT_BUILD)/psyad_targets.mk
 
-all: $(POST_PATCH_TARGETS) $(PSYAD_TARGETS) $(PRE_PATCH_TARGETS) 
+all: $(POST_PATCH_TARGETS) $(PSYAD_TARGETS) $(PRE_PATCH_TARGETS)
 
 # List of rules to build the adjoint kernels and adjoint test algorithms.
 include $(ADJOINT_BUILD)/pre_patch.mk
--- science/adjoint/build/import.mk
+++ science/adjoint/build/import.mk
@@ -6,9 +6,26 @@
 export PROJECT_SOURCE = $(APPS_ROOT_DIR)/science/adjoint/source
 
 .PHONY: import-adjoint
+import-adjoint: export ADJOINT_BUILD   := $(APPS_ROOT_DIR)/science/adjoint/build
+import-adjoint: export PSYAD_WDIR      := $(WORKING_DIR)/../psyad
+import-adjoint: export PATCH_DIR       := $(APPS_ROOT_DIR)/science/adjoint/patches
 import-adjoint:
+##############################################################################
+# Building PSyAD kernels
+	$Q$(MAKE) $(QUIET_ARG) -f $(ADJOINT_BUILD)/build_psyad.mk
+##############################################################################
+# Generating PSyADJT driver module file
+	$Q$(MAKE) $(QUIET_ARG) -f $(ADJOINT_BUILD)/psyad_driver.mk
+##############################################################################
+# Standard import commands
 	$Q$(MAKE) $(QUIET_ARG) -f $(LFRIC_BUILD)/extract.mk \
 	          SOURCE_DIR=$(PROJECT_SOURCE)
 	$Q$(MAKE) $(QUIET_ARG) -f $(LFRIC_BUILD)/psyclone/psyclone.mk \
 	          SOURCE_DIR=$(PROJECT_SOURCE) \
 	          OPTIMISATION_PATH=$(OPTIMISATION_PATH)
+##############################################################################
+# Need this step to PSyclone the generated adjoint tests from PSyAD
+	$Q$(MAKE) $(QUIET_ARG) -f $(LFRIC_BUILD)/psyclone/psyclone.mk \
+	          SOURCE_DIR=$(WORKING_DIR) \
+	          OPTIMISATION_PATH=$(OPTIMISATION_PATH)
+
